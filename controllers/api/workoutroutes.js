27de9a0b1bf81e9d4bdc5e8df6cd0676ff82e72f
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-15 00:59:57 -0400

    getting updated and total duration routes working

--- controllers/api/workoutroutes.js
+++ controllers/api/workoutroutes.js
@@ -1,6 +1,54 @@
 const router = require('express').Router();
 const db = require('../../models');
 
+router.get("/workouts", (req, res) => {
+    // empty object means no filter
+
+    // db.Workout.find({})
+    // .then(dbPizzaData=>{
+    //     res.json(dbPizzaData);
+    // })
+
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+    .then(dbPizzaData=>{
+        res.json(dbPizzaData);
+    })
+    
+})
+
+
+router.put("/workouts/:id", (req,res)=>{
+    db.Workout.findOneAndUpdate(
+        // finds a specific workout that matches the req.params.id
+        // the first paramater is filter
+        { _id: req.params.id }, 
+        // updating it with this: pushing the req.body into the workout's exercises array
+        {
+            $push: {
+                exercises: req.body
+            }
+        }, 
+        // return the updated value afterwards
+        { new: true}
+    ).then(dbPizzaData=>{
+        res.json(dbPizzaData);
+    })
+})
+
+
+
+
+
+
+
 
 
 
