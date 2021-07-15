35e8d448fa42ea4960fb7cca72dbaf5de5916d60
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-15 00:15:25 -0400

    server works and get routes to get for html files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const controlers = require("./controllers")
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(controlers);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// app.post("/submit", ({ body }, res) => {
+//   const user = new User(body);
+//   user.coolifier();
+//   user.makeCool();
+
+//   User.create(user)
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
